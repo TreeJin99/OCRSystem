2b865841de7b35ad34c8e17ff1fc8a7f15300e1e
남우진 <mokujin94@gmail.com>
2023-12-10 17:57:58 +0900

    git init

---
+++ app/src/main/java/com/android/ocrsystem/ui/activities/MainActivity.java
@@ -0,0 +1,16 @@
+package com.android.ocrsystem.ui.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.android.ocrsystem.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
